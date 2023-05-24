c3c56387e004c99f6006e3564df7b76bb390f940
RA4Z <rraz639@gmail.com>
2023-05-24 20:16:44 -0300

    Fazendo verificações no formulário

--- src/services/requisicoesFirebase.js
+++ src/services/requisicoesFirebase.js
@@ -1,12 +1,33 @@
 import { auth } from "../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, AuthErrorCodes } from "firebase/auth";
 
-export async function cadastrar(email, senha, confirmaSenha) {
-    createUserWithEmailAndPassword(auth, email, senha)
+function errosFirebase(erro) {
+    let mensagem = '';
+    switch(error.code) {
+        case AuthErrorCodes.EMAIL_EXISTS:
+            mensagem = "Esse E-mai já está em uso"
+            break;
+        case AuthErrorCodes.INVALID_EMAIL:
+            mensagem = "E-mail inválido"
+            break;
+        case AuthErrorCodes.WEAK_PASSWORD:
+            mensagem = "A senha precisa de no mínimo 6 caracteres"
+            break;
+        default:
+            mensagem="Erro desconhecido"
+    }
+    return mensagem
+}
+
+export async function cadastrar(email, senha) {
+    const resultado = await createUserWithEmailAndPassword(auth, email, senha)
     .then((dadosDoUsuario) => {
       console.log(dadosDoUsuario)
+      return "sucesso"
     })
     .catch((error) => {
       console.log(error)
+      return errosFirebase(error)
     });
+    return resultado;
 }

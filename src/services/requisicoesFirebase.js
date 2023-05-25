c3341f9d6d4e481d7bfc6abe0c83fc4718b0b4f4
RA4Z <rraz639@gmail.com>
2023-05-25 17:49:30 -0300

    Implementação de tela de login

--- src/services/requisicoesFirebase.js
+++ src/services/requisicoesFirebase.js
@@ -1,5 +1,5 @@
 import { auth } from "../config/firebase";
-import { createUserWithEmailAndPassword, AuthErrorCodes } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, AuthErrorCodes } from "firebase/auth";
 
 function errosFirebase(error) {
     let mensagem = '';
@@ -28,6 +28,20 @@ export async function cadastrar(email, senha) {
     .catch((error) => {
       console.log(error)
       return errosFirebase(error)
+    });
+    return resultado;
+}
+
+export async function logar(email, senha) {
+    const resultado = await signInWithEmailAndPassword(auth, email, senha)
+    .then((dadosDoUsuario) => {
+      console.log(dadosDoUsuario)
+      return "sucesso"
+    })
+    .catch((error) => {
+      console.log(error)
+      //return errosFirebase(error)
+      return "erro"
     });
     return resultado;
 }

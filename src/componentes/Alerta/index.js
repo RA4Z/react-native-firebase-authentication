68db820cf1be51d59103c8c6fb92b6a48cd5c43b
RA4Z <rraz639@gmail.com>
2023-05-25 17:44:50 -0300

    Desafio para declarar o tempo

--- src/componentes/Alerta/index.js
+++ src/componentes/Alerta/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Snackbar } from "react-native-paper";
 
-export function Alerta({ mensagem, error=false, SetError }) {
+export function Alerta({ mensagem, error=false, SetError, tempoDuracao }) {
     return (
         <Snackbar 
             visible={error}
             onDismiss={() => SetError(false)}
-            duration={1500}
+            duration={tempoDuracao}
             action={{
                 label: "OK",
                 onPress: () => SetError(false)
